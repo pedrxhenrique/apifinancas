759793f791a7f1bf092f7ca6ea939fe3374f76d2
Pedro <phj.silva@discente.ufma.br>
2025-05-26 02:44:20 -0300

    Initial commit

---
+++ src/main/java/io/github/financasapi/apifinancas/ApifinancasApplication.java
@@ -0,0 +1,13 @@
+package io.github.financasapi.apifinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/financasapi/apifinancas/ApifinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.financasapi.apifinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
